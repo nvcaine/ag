184253b4e8d610bbb6ef25139cfa1e1a5d6a083e
Romi Halasz <novocainex@gmail.com>
2013-06-11 15:38:21 +0300

    Explosion changes

--- src/entities/Explosion.hx
+++ src/entities/Explosion.hx
@@ -25,10 +25,15 @@ class Explosion extends Entity
 	{
 		var i:Int;
 
-		for(i in 0...500)
-		{
+		for(i in 0...200)
 			e.emit("exp", 32, 32);
-		}
+
+		for(i in 0...200)
+			e.emit("exp2", 32, 32);
+
+		for(i in 0...100)
+			e.emit("exp3", 32, 32);
+		
 			//e.emitInCircle("exp", this.x, this.y, 50);
 
 		var sound = Assets.getSound("sfx/explode.mp3");
@@ -40,9 +45,22 @@ class Explosion extends Entity
 		//e = new Emitter(new Bitmap(Assets.getBitmapData("gfx/particle.png")), 5, 5);//new BitmapData(5, 5, false, 0xFFFFFF), 5, 5);
 		e = new Emitter("gfx/particle.png", 31, 31);
 
+		// outter ring
 		e.newType("exp", [0]);
-		e.setMotion("exp", 0, 100, 0.5, 360, 100, 0.25, Ease.quadOut);
-		e.setAlpha("exp", 2, 0.5);
-		e.setColor("exp", 0xFFFF77, 0xFF0000, Ease.quadOut);
+		e.setMotion("exp", 0, 200, 0.5, 360, 10, 0.5, Ease.quadOut);
+		e.setAlpha("exp", 1, 0.15);
+		e.setColor("exp", 0x40ECF5, 0x2AA6DB, Ease.quadOut);
+
+		// mid-flame
+		e.newType("exp2", [0]);
+		e.setMotion("exp2", 0, 50, 0.5, 360, 100, 0.5, Ease.quadOut);
+		e.setAlpha("exp2", 2, 0.5);
+		e.setColor("exp2", 0xFFFB7D, 0xFF5917, Ease.quadOut);
+
+		// inner sparks
+		e.newType("exp3", [0]);
+		e.setMotion("exp3", 0, 25, 1, 360, 50, 1, Ease.quadOut);
+		e.setAlpha("exp3", 2, 0.05);
+		e.setColor("exp3", 0xFFFF77, 0xFF0000, Ease.quadOut);
 	}
 }
