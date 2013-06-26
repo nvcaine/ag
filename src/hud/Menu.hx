8ede1f0d012002ba0730c75b177baf0ab8883c44
nvcaine <novocainex@gmail.com>
2013-06-26 09:52:13 +0300

    Added Menu / MenuScene

---
+++ src/hud/Menu.hx
@@ -0,0 +1,11 @@
+package hud;
+
+import com.haxepunk.graphics.Canvas;
+import com.haxepunk.graphics.Graphiclist;
+import com.haxepunk.graphics.Text;
+import com.haxepunk.graphics.prototype.Rect;
+
+class Menu extends Graphicslist
+{
+	
+}
---
+++ src/scenes/MenuScene.hx
@@ -0,0 +1,12 @@
+package scenes;
+
+import com.haxepunk.HXP;
+import com.haxepunk.Scene;
+
+class GameScene extends Scene
+{
+	public function new()
+	{
+		super();
+	}
+}
